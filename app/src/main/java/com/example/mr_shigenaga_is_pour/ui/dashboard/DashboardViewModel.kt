165d8a7d4d057ed75757b92fa6fb7351adf97d5d
佐藤辰哉 <kd1371234@st.kobedenshi.ac.jp>
2024-06-26 14:29:03 +0900

    あほ

---
+++ app/src/main/java/com/example/mr_shigenaga_is_pour/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.mr_shigenaga_is_pour.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
